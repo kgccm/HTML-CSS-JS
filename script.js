78d3f572ab2d7e9782f3ca89411097ab04778c81
kgccm <990720wlgns@naver.com>
2023-07-06 21:24:42 +0900

    .

---
+++ script.js
@@ -0,0 +1,4 @@
+// var clothes = document.getElementById('clothes');
+// function mouseover(){
+//     clothes.setAttribute("src", "https://www.jilsander.com/dw/image/v2/BGNJ_PRD/on/demandware.static/-/Sites-jilsander-master-catalog/default/dw544b9c38/images/large/J02NC0166_J14511_825_F.jpg?sw=750&sh=1125")
+// }
